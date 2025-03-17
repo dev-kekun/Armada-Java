3b28b4ca07abd148dc17452dccf64d7eba013b9e
Rikki <dev.davidricky@gmail.com>
2025-03-17 20:21:01 +0800

    Task I: Arithmetic Operations Complete

---
+++ Assignment_Operators_I/arithmeticOperatorsRCD.java
@@ -0,0 +1,18 @@
+package Assignment_Operators_I;
+
+public class arithmeticOperatorsRCD {
+    public static void main(String[] args){
+        int num1 = 10;
+        int num2 = 5;
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        int product = num1 * num2;
+        int quotient = num1 / num2;
+
+        System.out.println(num1 + " + " + num2 + " = " + sum );
+        System.out.println(num1 + " - " + num2 + " = " + difference );
+        System.out.println(num1 + " * " + num2 + " = " + product );
+        System.out.println(num1 + " / " + num2 + " = " + quotient );
+        
+    }
+}
