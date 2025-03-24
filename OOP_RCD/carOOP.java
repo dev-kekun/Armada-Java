54501cb5b618c626b6bf30c58e406c297d44eb8d
Rikki <dev.davidricky@gmail.com>
2025-03-25 03:21:12 +0800

    Car Object Complete

---
+++ OOP_RCD/carOOP.java
@@ -0,0 +1,23 @@
+package OOP_RCD;
+
+class Car {
+
+    String brand;
+    int year;
+
+    void displayInfo(){
+        System.out.println("Brand: " + brand);
+        System.out.println("Year: " + year);
+    }
+}
+
+public class carOOP {
+    public static void main(String[] args){
+
+        Car car1 = new Car();
+        car1.brand = "Toyota";
+        car1.year = 2020;
+
+        car1.displayInfo();
+    }
+}
